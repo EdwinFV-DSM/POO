b06109d82a2d778291ed55801f533fade29faeea
Edwin <al222110833@gmail.com>
2022-04-05 21:43:02 -0500

    Modificacion de generar compra

--- Proyecto/cliente.java
+++ Proyecto/cliente.java
@@ -1,4 +1,5 @@
 package Proyecto;
+import java.io.*;
 
 public class cliente extends Persona {
 
@@ -10,21 +11,14 @@ public class cliente extends Persona {
     }
     
     public String generarComprar(){
-        String cont = "exit";
-        double su;
-				while (cont == "") {
-					System.out.println("--Esta en el apartado de Sumatoria de Compras--");
-					System.out.println("Ingrese la cantidad del producto "+cont+" a comprar,");
-					Object bufEntrada;
-                    double art = Double.parseDouble(((Object) bufEntrada).readLine());
-					System.out.println("Ingrese el precio del producto "+cont+" a comprar");
-					Double pre = Double.parseDouble(((Object) bufEntrada).readLine());
-					double sto = art*pre*1.16;
-					System.out.println("El subtotal de la compra es: $"+sto);
-					cont = cont+cont;
-					su = su+sto;
-                }
-                return "Tu subtotal es: "+su;
+        String respuesta = "exit";
+        String msg = "";
+       while (respuesta == " ") {
+           
+       }
+       msg = "Funcionando";
+
+       return msg;
     }
 
     public String mostrarDatos(){
