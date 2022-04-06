99feb64e3d8b2d131c0220af34f39c751a37c364
Edwin <al222110833@gmail.com>
2022-04-05 22:54:52 -0500

    Sueldo modificacion

--- Proyecto/Empleados.java
+++ Proyecto/Empleados.java
@@ -30,13 +30,37 @@ public class Empleados extends Persona {
     }
 
     public String obtenerSueldo(){
-        return "Tu sueldo es 0";
+        String msg = "";
+        int ht = 0;
+        int precio = 0;
+
+        if (ht <= 30) {
+            int res1;
+            res1= ht * precio;
+            msg = "Tu nombre es: "+nombre+"\n Tu salario es el siguiente: "+res1;
+        }else if (ht >= 31 && ht < 40){
+            int res2,res4;
+            res2 = ht - 30;
+            ht = ht - res2;
+            int valor = res2*precio;
+            msg = "Tu nombre es: "+nombre+"\n Tu salario es el siguiente con horas extras: "+res2*2;
+        }else if(ht >= 40){
+            int res3,res4;
+            
+            res3 = ht - 40;
+            ht = ht - res3;
+            int valor = res3*precio;
+            msg = "Tu nombre es: "+nombre+"\n Tu salario es el siguiente con horas extras: "+valor*3;
+        }else{
+            msg = "No has cumplido con las horas trabajadas solicitadas";
+        }
+        return msg;
     }   
     
     public String validarTurno(){
         String msg = "";
 
-        if (turno == "") {
+        if (turno == "" || turno == " ") {
             turno = "Matutino";
             msg = turno;
         }else{
