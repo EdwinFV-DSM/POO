1f8220fcb0f0f5da81fb8b6f366d78d974ca57e5
Edwin <al222110833@gmail.com>
2022-04-06 01:41:40 -0500

    Agregacion de librerias

--- Proyecto/pisocajeros.java
+++ Proyecto/pisocajeros.java
@@ -1,32 +1,49 @@
 package Proyecto;
 
-public class pisocajeros extends Empleados {
+import java.util.Scanner;
+
+public class pisocajeros extends Persona {
     String catego;
 
     public pisocajeros() {
     }
 
-    public pisocajeros(char sexo, String telefono, String fechaNac, String nombre, String departamento, String turno,
-            String catego) {
-        super(sexo, telefono, fechaNac, nombre, departamento, turno);
+    public pisocajeros(char sexo, String telefono, String fechaNac, String nombre, String catego) {
+        super(sexo, telefono, fechaNac, nombre);
         this.catego = catego;
     }
 
     public String getCatego() {
         return catego;
     }
-
+    
     public void setCatego(String catego) {
         this.catego = catego;
     }
 
-    public String obtenerSueldo(){
-        
+    public String obtenerSueldo() {
+        int ht;
+        String msg = "";
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Introduce las horas trabajadas:");
+            ht = sc.nextInt();
+        }
+        if (catego == "piso" && ht <= 8) {
+            int res1;
+            res1 = ht * 8;
+            res1 = res1 * 10;
+            msg = "" + res1;
+
+        } else {
+            msg = "El area ingresada no se encuentra";
+        }
+        return msg;
     }
 
-    public String mostrarDatos(){
+    @Override
+    public String mostrarDatos() {
         String msg = "";
-        msg = super.mostrarDatos()+"\nTu turno es: "+turno+"\nTu sueldo es: "+obtenerSueldo()+"\nTu departamento es: "+Departamento;
+        msg = super.mostrarDatos() + "\nTu salario es el siguiente: " + obtenerSueldo() + "\nTu departamento es: " + catego;
         return msg;
     }
 }
