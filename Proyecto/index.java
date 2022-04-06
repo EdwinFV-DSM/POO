da9d4091bce28e706c231e2be1e9b9a499b9ad16
Edwin <al222110833@gmail.com>
2022-04-05 21:44:09 -0500

    Solicitacion de recursos

--- Proyecto/index.java
+++ Proyecto/index.java
@@ -1,5 +1,13 @@
 package Proyecto;
 
 public class index {
-    
+    public static void main(String[] args) {
+        Persona per = new Persona();
+        Persona per2 = new Persona('M', "7221090131","30/07/03","Edwin Flores");
+        System.out.println(per2.mostrarDatos());
+
+        Empleados EM = new Empleados('F', "7221090131","30/07/03","Johana","Recursos humanos","Vespertino");
+        EM.validarTurno();
+        System.out.println(EM.mostrarDatos());
+    }
 }
