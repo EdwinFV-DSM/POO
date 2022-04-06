60d37e40050f51236ababd7e76422913459f7978
Edwin <al222110833@gmail.com>
2022-04-06 01:42:14 -0500

    creacion de personas

--- Proyecto/index.java
+++ Proyecto/index.java
@@ -2,12 +2,18 @@ package Proyecto;
 
 public class index {
     public static void main(String[] args) {
-        Persona per = new Persona();
-        Persona per2 = new Persona('M', "7221090131","30/07/03","Edwin Flores");
-        System.out.println(per2.mostrarDatos());
+        // Persona per = new Persona();
+        // Persona per2 = new Persona('M', "7221090131","30/07/03","Edwin Flores");
+        // System.out.println(per2.mostrarDatos());
 
-        Empleados EM = new Empleados('F', "7221090131","30/07/03","Johana","Recursos humanos"," ");
-        EM.validarTurno();
-        System.out.println(EM.mostrarDatos());
+        //Empleados
+        //  Empleados EM = new Empleados('F', "7221090131","30/07/03","Johana","Recursos humanos","Vespertino");
+        //  EM.validarTurno();
+        //  System.out.println(EM.mostrarDatos());
+        //pisocajeros8
+        
+        pisocajeros piso = new pisocajeros('F', "7221090131","30/07/03","Johana","piso");
+        piso.obtenerSueldo();
+        System.out.println(piso.mostrarDatos());
     }
 }
